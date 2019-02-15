184e7685df73ce0e20b55d24d2491fc68de6daef
Cameron Dimitroff <cdimitro@e1z2r7p7.42.us.org>
2019-02-15 12:42:08 -0800

    update str~cap

--- 3_level/str_capitalizer.c
+++ 3_level/str_capitalizer.c
@@ -5,50 +5,85 @@ void ft_putchar(char c)
 	write(1, &c, 1);
 }
 
+void ft_putstr(char *s)
+{
+	int i = 0;
+	while (s[i] != '\0')
+	{
+		ft_putchar(s[i]);
+		i++;
+	}
+}
+
+int is_char(char c)
+{
+	if (c >= 33 && c <= 126)
+		return (1);
+	return (0);
+}
+
+int	is_upper(char c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
+
+int is_lower(char c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
+
+int space_or_tab(char c)
+{
+	if (c == '\t' || c == ' ')
+		return (1);
+	return (0);
+}
+
+char to_upper(char c)
+{
+	return (c - 32);
+}
+
+char to_lower(char c)
+{
+	return (c + 32);
+}
+
+void ft_putstr_capitalizer(char *s)
+{
+	int i = 0;
+
+	while (is_char(s[i]) != 1)
+		i++;
+	if (is_lower(s[i]) == 1)
+		s[i] = to_upper(s[i]);
+	while (s[i] != '\0')
+	{
+		if (is_lower(s[i]) == 1 && space_or_tab(s[i - 1])== 1)
+			s[i] = to_upper(s[i]);
+		else if (is_upper(s[i]) == 1 && is_char(s[i - 1]) == 1)
+			s[i] = to_lower(s[i]);
+		else
+			s[i] = s[i];
+		i++;
+	}
+	ft_putstr(s);
+}
+
 int		main(int ac, char **av)
 {
+
 	if (ac > 1)
 	{
 		int i = 1;
-		int j = 0;
-
-		while (av[i] != av[ac])
+		while (i < ac)
 		{
-			if (av[i][0] <= 90 && av[i][0] >= 65)
-			{
-				ft_putchar(av[i][j]);
-				j++;
-			}
-			if (av[i][0] <= 122 && av[i][0] >= 97)
-			{
-				ft_putchar(av[i][j] - 32);
-				j++;
-			}
-			while (av[i][j] != '\0')
-			{
-				if (av[i][j] <= 90 && av[i][j] >= 65 && (av[i][j-1] == '\t' || av[i][j-1] == ' '))
-				{
-			  		ft_putchar(av[i][j]);
-					j++;
-				}
-				else if (av[i][j] <= 90 && av[i][j] >= 65)
-				{
-					ft_putchar(av[i][j] + 32);
-					j++;
-				}
-				else if (av[i][j] <= 122 && av[i][j] >= 97 && (av[i][j-1] == '\t' || av[i][j-1] == ' '))
-				{
-					ft_putchar(av[i][j] - 32);
-					j++;
-				}
-				else
-				{
-					ft_putchar(av[i][j]);
-					j++;
-				}
-			}
+			ft_putstr_capitalizer(av[i]);
 			ft_putchar('\n');
-			j = 0;
 			i++;
 		}
 	}
