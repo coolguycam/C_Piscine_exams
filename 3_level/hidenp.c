f5b9b32eda1a6da8212ddf7f58450740df9878f9
Cameron Dimitroff <cdimitro@e1z2r9p4.42.us.org>
2019-02-25 21:41:16 -0800

    hidnep

--- 3_level/hidenp.c
+++ 3_level/hidenp.c
@@ -5,12 +5,36 @@ void ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-int hidenp(char *s1, char *st2)
+int	wdmatch(char *s1, char *s2)
 {
-	int i = 0;
+	int i;
+	int j;
 
+	i = 0;
+	j = 0;
 	while (s2[i] != '\0')
 	{
+		if (s2[i] == s1[j])
+			j++;
+		i++;
+	}
+	if (s1[j] == '\0')
+		return (1);
+	return (0);
+}
 
-	}	
+void ft_putnbr(int n)
+{
+	if (n == 1)
+		ft_putchar('1');
+	else if (n == 0)
+		ft_putchar('0');
+}
+
+int main(int c, char **v)
+{
+	if (c == 3)
+		ft_putnbr(wdmatch(v[1], v[2]));
+	ft_putchar('\n');
+	return (0);
 }
